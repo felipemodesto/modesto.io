7350f29c7505e2c254d696bc0c528b78bf7f84ad
Felipe Modesto <felipe.modesto@firasoft.com>
2018-10-31 23:04:28 -0400

    Reverting changes to ports

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ app.debug = False
 
 #Note: the secret_key should be generated randomly using some seed and be kept away from people and github public repos (In the future, for now it doesn't really matter :D)
 app.secret_key = 'supersecretpasswordwewillneverdisclosecauseyougottagitgudlikeover9000tripleunderscore__'
-port = int(os.environ.get("PORT", 5050))
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
 	#app.run(host='127.0.0.1',port=port, use_reloader=True, ssl_context=('cert.pem','privkey.pem'))
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --worker-class gevent -w 1 --timeout 600 --log-level=debug --bind 127.0.0.1:5050 --certfile=ssl/cert.pem --keyfile=ssl/privkey.pem run:app
+gunicorn --worker-class gevent -w 1 --timeout 600 --log-level=debug --bind 127.0.0.1:5000 --certfile=ssl/cert.pem --keyfile=ssl/privkey.pem run:app
