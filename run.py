0d1b05e358dcbdbe66fb6bb1a0a9cd0b7334111a
Felipe Modesto <felipe.moraes.modesto@gmail.com>
2018-11-02 17:54:40 -0400

    Adding updated StreamBeats page with Heart Rate List, integrating link into main page as well as some other minor improvements.

--- run.py
+++ run.py
@@ -7,10 +7,9 @@ from flask_sslify import SSLify
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
-context = ('ssl/cert.pem','ssl/privkey.pem')
-
-sslify = SSLify(app)
-app.debug = False
+#context = ('ssl/cert.pem','ssl/privkey.pem')
+#sslify = SSLify(app)
+#app.debug = False
 
 #Note: the secret_key should be generated randomly using some seed and be kept away from people and github public repos (In the future, for now it doesn't really matter :D)
 app.secret_key = 'supersecretpasswordwewillneverdisclosecauseyougottagitgudlikeover9000tripleunderscore__'
@@ -18,7 +17,7 @@ port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
 	#app.run(host='127.0.0.1',port=port, use_reloader=True, ssl_context=('cert.pem','privkey.pem'))
-	app.run(host='0.0.0.0',port=port, use_reloader=True,threaded=True,ssl_context=context)
-	#app.run(host='0.0.0.0',port=port, use_reloader=True,threaded=True)#,ssl_context=context)
+    #app.run(host='0.0.0.0',port=port, use_reloader=True,threaded=True,ssl_context=context)
+    app.run(host='0.0.0.0',port=port, use_reloader=True,threaded=True)
 
 print ">> Run File Loaded."
