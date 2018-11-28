8dad6a9053bf500eb3180789b218b781673ca4e8
Felipe Modesto <felipe.moraes.modesto@gmail.com>
2018-11-27 21:02:05 -0500

    working on CORS errors

--- run.py
+++ run.py
@@ -4,12 +4,19 @@ from app import app
 
 from flask import Flask
 from flask_sslify import SSLify
+from flask_cors import CORS
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 #context = ('ssl/cert.pem','ssl/privkey.pem')
 #sslify = SSLify(app)
 #app.debug = False
+
+#CORS(app, origins="*", allow_headers=[
+#    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+#    supports_credentials=True)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 #Note: the secret_key should be generated randomly using some seed and be kept away from people and github public repos (In the future, for now it doesn't really matter :D)
 app.secret_key = 'supersecretpasswordwewillneverdisclosecauseyougottagitgudlikeover9000tripleunderscore__'
