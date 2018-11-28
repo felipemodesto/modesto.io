cbd109cd2b3a5165b3cd43e796578efcab72e66a
Felipe Modesto <felipe.modesto@firasoft.com>
2018-11-27 21:01:40 -0500

    Updated minesweeper client request urls

---
+++ app/static/precache-manifest.33d0d525476b83286a5c952d09c73e21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a164bfb152b8b7a655fd",
+    "url": "/static/js/main.a164bfb1.chunk.js"
+  },
+  {
+    "revision": "95b4440f1dfd67c8a3a9",
+    "url": "/static/js/1.95b4440f.chunk.js"
+  },
+  {
+    "revision": "a164bfb152b8b7a655fd",
+    "url": "/static/css/main.2b9a8d0d.chunk.css"
+  },
+  {
+    "revision": "95b4440f1dfd67c8a3a9",
+    "url": "/static/css/1.67f8f271.chunk.css"
+  },
+  {
+    "revision": "6cb35fb86b21a13a1ed21badc47e5acd",
+    "url": "/index.html"
+  }
+];
