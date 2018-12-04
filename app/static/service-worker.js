ef582ae040ff1f7611a5edf445240956215afaf6
Felipe Modesto <felipe.modesto@firasoft.com>
2018-12-04 00:08:26 -0500

    Updating minesweeper page title & fixing missing file (attempt)

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb43d4f83a95281d4bc4379688541fc1.js"
+  "/precache-manifest.99032ce4e321a828afd137e806372623.js"
 );
 
 workbox.clientsClaim();
